3177f3b98502e0ab24e3e5518ec3e7b3af2729c3
Adriano de Oliveira <adriano.98@gmail.com>
2022-03-16 14:55:42 -0300

    Resolvendo bug ao add participante com espaços vazios, ele aceitava. E também se clicasse em sorteio com a lista vazia, ficava em loop o spin. Adicionado também o botão 'limpar' para apagar todos os nomes.

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ window.addEventListener("load", () => {
   const input = document.querySelector("#participantes");
   const lista = document.querySelector("#lista");
   const main = document.querySelector("#main");
-  const boxInitial = document.querySelector("#initialbox")
+  const boxInitial = document.querySelector("#initialbox");
   const erro = document.querySelector("#erro");
+  const erroSorteio = document.querySelector("#errosorteio");
   const buttonSorteio = document.querySelector("#buttonsorteio");
+  const erroReset = document.querySelector("#erroreset");
   const loadingResult = document.querySelector(".loading");
-
 
   const validaParticipante = () => {
     input.addEventListener("input", (event) => {
       erro.style.display = "none";
+      erroSorteio.style.display = "none";
+      erroReset.style.display = "none";
     });
   };
 
@@ -20,10 +23,11 @@ window.addEventListener("load", () => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const pessoa = input.value;
+    const pessoa = input.value.trim();
 
     if (!pessoa) {
       erro.style.display = "block";
+      erroSorteio.style.display = "none";
       return;
     }
 
@@ -40,7 +44,6 @@ window.addEventListener("load", () => {
     listaParticipantes.appendChild(participantesAdicionados);
     listaParticipantes.appendChild(buttonremove);
     lista.appendChild(listaParticipantes);
-  
 
     input.value = "";
 
@@ -52,33 +55,51 @@ window.addEventListener("load", () => {
   buttonSorteio.addEventListener("click", () => {
     loadingResult.style.display = "flex";
 
+    if (lista.childNodes.length < 2) {
+      erroSorteio.style.display = "block";
+      loadingResult.style.display = "none";
+      return;
+    }
+
     const vencedor = Math.floor(Math.random() * lista.childNodes.length);
-    console.log(vencedor);
+    // console.log(vencedor);
     const pessoa = lista.childNodes[vencedor].firstChild.innerText;
-    console.log(pessoa);
+    // console.log(pessoa);
+    // console.log(lista.childNodes.length);
 
-    const popup = document.querySelector(".popup-wrapper")
-    const anuncioVencedorTexto = document.querySelector(".popup-content")
+    const popup = document.querySelector(".popup-wrapper");
+    const anuncioVencedorTexto = document.querySelector(".popup-content");
 
-    console.log(anuncioVencedorTexto.childNodes);
-    console.log(anuncioVencedorTexto.childNodes[3])
-    console.log(anuncioVencedorTexto.childNodes[3].childNodes[0]);
+    // console.log(anuncioVencedorTexto.childNodes);
+    // console.log(anuncioVencedorTexto.childNodes[3])
+    // console.log(anuncioVencedorTexto.childNodes[3].childNodes[0]);
 
     const popupText = anuncioVencedorTexto.childNodes[3].childNodes[0];
 
-    popupText.innerText = pessoa
+    popupText.innerText = pessoa;
 
     setTimeout(() => {
-      popup.style.display='flex';
-      loadingResult.style.display="none";
-  },
-   3000)
+      popup.style.display = "flex";
+      loadingResult.style.display = "none";
+    }, 3000);
 
-   const fecharPopUp = document.querySelector(".material-icons")
+    const fecharPopUp = document.querySelector(".material-icons");
 
-   fecharPopUp.addEventListener('click', () => {
-     popup.style.display='none'
-   })
+    fecharPopUp.addEventListener("click", () => {
+      popup.style.display = "none";
+    });
   });
+  const buttonLimpar = document.querySelector("#limparNomes");
 
+  const limparTodosNomes = () => {
+    lista.innerText = "";
+  };
+
+  buttonLimpar.addEventListener("click", () => {
+    if (lista.childNodes.length === 0) {
+      erroReset.style.display = "block";
+      return;
+    }
+    limparTodosNomes();
+  });
 });
